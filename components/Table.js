cf7c89ca67f54e33ffe8cc6946db8614252dbde9
JIeCoRyI3 <jiecoryi3@gmail.com>
2020-02-21 16:08:33 +0200

    Add real change calculation

--- components/Table.js
+++ components/Table.js
@@ -2,39 +2,53 @@ import React from 'react';
 import '../styles/application.scss';
 import PropTypes from 'prop-types';
 
-function Table(props) {
-    const date = props.res.last_trade_time.slice(0, 10);
-    const time = props.res.last_trade_time.slice(11, 19);
-    const animation = ' animated flash delay-2s';
-    let cls = Number(props.res.change) >= 0 ? 'fas fa-arrow-circle-up' : 'fas fa-arrow-circle-down';
-    cls += animation;
-    return(
-        <div>
-            <h1>Table</h1>
-            <table className="table table-hover">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Ticker</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Change</th>
-                    <th scope="col">Change percent</th>
-                    <th scope="col">Last trade time</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>{props.res.ticker}</td>
-                    <td>{props.res.price} <i className={cls}></i></td>
-                    <td>{props.res.change} <i className={cls}></i></td>
-                    <td>{props.res.change_percent} <i className={cls}></i></td>
-                    <td>{date[0] === 'T' ? props.res.last_trade_time : date + ' ' + time}</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+class Table extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {changes: 0};
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.res.price !== this.props.res.price) {
+            const change = nextProps.res.price - this.props.res.price;
+            this.setState({changes: change });
+            // console.log( 'WOW!!!! ' + change);
+        }
+    }
+    render() {
+        const date = this.props.res.last_trade_time.slice(0, 10);
+        const time = this.props.res.last_trade_time.slice(11, 19);
+        const animation = ' animated flash delay-2s';
+        let cls = Number(this.state.changes) >= 0 ? 'fas fa-arrow-circle-up' : 'fas fa-arrow-circle-down';
+        cls += animation;
+        return(
+            <div>
+                <h1>Table</h1>
+                <table className="table table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Ticker</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Change</th>
+                        <th scope="col">Change percent</th>
+                        <th scope="col">Last trade time</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>{this.props.res.ticker}</td>
+                        <td>{this.props.res.price} <i className={cls}></i></td>
+                        <td>{this.state.changes.toFixed(2)} <i className={cls}></i></td>
+                        <td>{this.props.res.change_percent} <i className={cls}></i></td>
+                        <td>{date[0] === 'T' ? this.props.res.last_trade_time : date + ' ' + time}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 Table.propTypes = {
